10636a149127736763542875b2609c871e5bed9b
waelalq <wael.abuomr@gmail.com>
2022-01-25 19:37:52 +0200

    add style and event

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HR-management-system
+# 25/1/2022 I add style for the web page and create daynamic web 
+- i think the taske will need 2 hour but it take +4 hour
--- js/app.js
+++ js/app.js
@@ -10,44 +10,82 @@ function Employee(employeeId,fullName,department,level,imageUrl,salary)
 Employee.prototype.salary=function(){
     if(this.level=="Senior"){ 
        
-        this.selary=Math.random() * (2000 - 1500) + 1500;
+        this.selary=Math.floor(Math.random() * (2000 - 1500) + 1500);
         this.salary=this.salary-(this.salary*.075);
-        document.write(this.fullName+" " + this.selary + "<br>");
+        creatCard(this.fullName,this.imageUrl,this.level,this.department,this.employeeId);
    
 }
 
 if(this.level=="Mid-Senior"){ 
  
-    this.selary = Math.random() * (1500 - 1000) + 1000;
+    this.selary = Math.floor(Math.random() * (1500 - 1000) + 1000);
     this.salary=this.salary-(this.salary*.075);
-    document.write(this.fullName+" " + this.selary + "<br>");
+    creatCard(this.fullName,this.imageUrl,this.level,this.department,this.employeeId);
+ 
 }
 
 
 if(this.level=="Junior"){ 
-    this.selary= Math.random() * (1000 - 500) + 500;
+    this.selary= Math.floor(Math.random() * (1000 - 500) + 500);
     this.salary=this.salary-(this.salary*.075);
-    document.write(this.fullName+" " + this.selary + "<br>");
+    creatCard(this.fullName,this.imageUrl,this.level,this.department,this.employeeId);
+    // document.write(this.fullName+" " + this.selary + "<br>");
     }
   
 }
-const GhaziSamer = new Employee(1000,"Ghazi Samer","Administration","Senior","./img/Ghazi.png");
+const GhaziSamer = new Employee(1000,"Ghazi Samer","Administration","Senior","./img/Ghazi .png");
 GhaziSamer.salary();
 
-const lanaAli = new Employee(1001,"Lana Ali","Finance","Senior","./img/Lana.png");
+const lanaAli = new Employee(1001,"Lana Ali","Finance","Senior","./img/Lana .png");
 lanaAli.salary();
 
-const tamaraAyoub = new Employee(1002,"Tamara Ayoub","Marketing","Senior","./img/tamar.png");
+const tamaraAyoub = new Employee(1002,"Tamara Ayoub","Marketing","Senior","./img/Tamara .png");
 tamaraAyoub.salary();
 
-const safiWalid = new Employee(1003,"Safi Walid","Administration","Mid-Senior","./img/safi.png");
+const safiWalid = new Employee(1003,"Safi Walid","Administration","Mid-Senior","./img/Safi.png");
 safiWalid.salary();
 
-const omarZaid = new Employee(1004,"Omar Zaid","Development","Senior","./img/Omar.png");
+const omarZaid = new Employee(1004,"Omar Zaid","Development","Senior","./img/Omar .png");
 omarZaid.salary();
 
-const ranaSaleh = new Employee(1004,"Rana Saleh","Development","Junior","./img/Rana.png");
+const ranaSaleh = new Employee(1004,"Rana Saleh","Development","Junior","./img/Rana .png");
 ranaSaleh.salary();
 
-const hadiAhmad = new Employee(1004,"Hadi Ahmad","Finance","Mid-Senior","./img/hadi.png");
+const hadiAhmad = new Employee(1004,"Hadi Ahmad","Finance","Mid-Senior","./img/Hadi .png",);
 hadiAhmad.salary();
+
+///////////////////////////////////////////
+var continer=document.getElementById("continer");
+let form =document.getElementById("form");
+function addPerson(event){
+  event.preventDefault();
+  let  name=event.target.fullName.value;
+  let imgUrl=event.target.imgUrl.value;
+  let level=event.target.level.value;
+  let Department=event.target.Department.value;
+  
+  console.table(name,imgUrl,level,Department,idGenrator());
+  creatCard(name,imgUrl,level,Department,idGenrator());
+}
+
+
+function idGenrator()
+{let id =Math.floor(Math.random() * 10000);
+    return id;
+}
+
+function creatCard(name,imgUrl,level,Department ,id){
+    var continer=document.getElementById("continer");
+   let div= document.createElement('div');
+   let contant= document.createElement('div');
+   let img= document.createElement('img');
+   div.appendChild(img);
+   img.setAttribute('src',imgUrl);
+   let card= continer.appendChild(div);
+card.appendChild(contant);
+contant.textContent=` Name:${name} -Level:${level} Department:${Department}-ID: ${id}`;
+    div.setAttribute('class','card');
+  
+
+}
+form.addEventListener('submit', addPerson);
