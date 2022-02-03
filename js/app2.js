88a4c532e4faed8ebc48043abb479c8b24b7fb66
waelalq <wael.abuomr@gmail.com>
2022-02-03 12:25:33 +0200

    all notes

--- js/app2.js
+++ js/app2.js
@@ -53,27 +53,68 @@ let allEmployySal=0;
             
         }
 
-        document.getElementById('allEmployy').innerHTML=allEmployy;
-        document.getElementById('allEmployySal').innerHTML=allEmployySal;
-        document.getElementById('allEmployySalA').innerHTML=allEmployySal/allEmployy;
-       
-        document.getElementById('MarketingCount').innerHTML=MarketingCount;
-        document.getElementById('FinanceCOunt').innerHTML=FinanceCOunt;
-       
-        document.getElementById('DevelopmentCount').innerHTML=DevelopmentCount;
-        document.getElementById('adminNamber').innerHTML=adminCount;
-        document.getElementById('Marketing').innerHTML=Marketing;
-        document.getElementById('Finance').innerHTML=Finance;
-       
-        document.getElementById('Development').innerHTML=Development;
-        document.getElementById('Administration').innerHTML=Administration;
-      
+        
+        let table = document.createElement('table');
+        
+        let table2 = document.createElement('table');
+        document.getElementById('main').appendChild(table2);
+        document.getElementById('main').appendChild(table);
 
-        document.getElementById('MarketingA').innerHTML=Marketing/MarketingCount;
-        document.getElementById('FinanceA').innerHTML=Finance/FinanceCOunt;
+
+        createDep("th", " Department","Number of employees","Total salary",table2);
+        createDep("td", "All Department",allEmployy,allEmployySal,table2);
+        
+        createDep("th", "Department Name","Number of employees","Total salary",table);
+        createDep("td", "Finance",FinanceCOunt,Finance,table);
+        createDep("td", "Development",DevelopmentCount,Development,table);
+        createDep("td", "Marketing",MarketingCount,Marketing,table);
+
+        createDep("td", "Administration",adminCount,Administration,table);
        
-        document.getElementById('DevelopmentA').innerHTML=Development/DevelopmentCount;
-        document.getElementById('AdministrationA').innerHTML=Administration/adminCount;
+
+ function createDep(sty,f1,f2,f3,tableN){
+let c=0;
+
+            let tr1 = document.createElement('tr');
+            let th1 = document.createElement(sty);
+            th1.setAttribute("id", c++);
+            th1.innerHTML =f1;
+            let th2= document.createElement(sty);
+            th2.innerHTML =f2 ;
+            let th3 = document.createElement(sty);
+            th3.innerHTML = f3;
+            let th4 = document.createElement(sty);
+            if (sty=="th") {
+                th4.innerHTML ="Average salary" ;   
+            }
+            else
+            {
+                th4.innerHTML =f3/nanHandel(f2);    
+            }
+          
+    
+            tr1.appendChild(th1);
+            tr1.appendChild(th2);
+            tr1.appendChild(th3);
+            tr1.appendChild(th4);
+            tableN.appendChild(tr1);
+            
+
+
+        }
+
+        function nanHandel(n1){
+      
+            if (n1==0||n1==null||n1==NaN) {
+                return 1;
+
+                
+            }
+            else{
+                return n1  
+            }
+        }
+       
       
         }
 
